ac8642f2f2438de8d1b554d4076fd42d347bfa9c
Bahri Enis Demirtel <bahrienisdemirtel09@gmail.com>
2017-05-28 16:33:10 +0200

    First commit

---
+++ build/opencv_tutorial/CMakeFiles/opencv_tutorial_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_tutorial_node.dir/src/internal_cam_test.cpp.o"
+  "/home/enis/catkin_ws/devel/lib/opencv_tutorial/opencv_tutorial_node.pdb"
+  "/home/enis/catkin_ws/devel/lib/opencv_tutorial/opencv_tutorial_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_tutorial_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
